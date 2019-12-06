0c76d333690af5047868bdf249541b48446e6e62
Marat Abrarov <abrarov@gmail.com>
2019-12-06 14:27:41 +0300

    Fixed usage prompt in RLE

--- rle/src/main/java/org/mabrarov/yatest/rle/Main.java
+++ rle/src/main/java/org/mabrarov/yatest/rle/Main.java
@@ -20,7 +20,7 @@ public class Main {
   public static void main(String[] args) {
     if (args.length < 1) {
       // No text to transform is specified - show usage and ext with error
-      System.err.println("Usage: java -jar yatest.jar text_to_transform");
+      System.err.println("Usage: java -jar rle.jar text_to_transform");
       System.exit(1);
     }
     String input = args[0];
