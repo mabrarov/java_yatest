8271212a0ff24abcda50a09b151aaba179cbe937
Marat Abrarov <abrarov@gmail.com>
2019-12-06 16:12:04 +0300

    Robot test

---
+++ robot/README.md
@@ -0,0 +1,20 @@
+# Robot
+
+Makes steps foot by foot starting from right foot, i.e. prints
+
+```text
+Step by right
+Step by left
+Step by right
+...
+```
+
+## Usage
+
+Assuming current directory is directory where this README is located.
+
+```bash
+java -jar target/robot-0.0.1-SNAPSHOT.jar
+```
+
+Press Ctrl+C to stop.
---
+++ robot/src/main/java/org/mabrarov/yatest/robot/Robot.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2019 Marat Abrarov (abrarov@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mabrarov.yatest.robot;
+
+import java.util.concurrent.Semaphore;
+
+class Foot implements Runnable {
+
+  private final Semaphore mayStepSemaphore;
+  private final Semaphore stepMadeSemaphore;
+  private final String name;
+
+  public Foot(String name, Semaphore mayStepSemaphore, Semaphore stepMadeSemaphore) {
+    this.name = name;
+    this.mayStepSemaphore = mayStepSemaphore;
+    this.stepMadeSemaphore = stepMadeSemaphore;
+  }
+
+  public void run() {
+    for (; ; ) {
+      try {
+        step();
+      } catch (InterruptedException ignored) {
+        Thread.currentThread().interrupt();
+        break;
+      }
+    }
+  }
+
+  private void step() throws InterruptedException {
+    mayStepSemaphore.acquire();
+    System.out.println("Step by " + name);
+    stepMadeSemaphore.release();
+  }
+}
+
+public class Robot {
+
+  public static void main(String[] args) {
+    // First step by right foot
+    final Semaphore leftStepMade = new Semaphore(1);
+    final Semaphore rightStepMade = new Semaphore(0);
+    new Thread(new Foot("left", rightStepMade, leftStepMade)).start();
+    new Thread(new Foot("right", leftStepMade, rightStepMade)).start();
+
+  }
+}
