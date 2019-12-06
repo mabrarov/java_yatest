cbd5d2c99453c00730e6d7a22b9cf543fb837660
Marat Abrarov <abrarov@gmail.com>
2019-12-06 12:07:58 +0300

    Initial try to fix CI script for Codecov

--- scripts/appveyor/codecov.ps1
+++ scripts/appveyor/codecov.ps1
@@ -1,8 +1,9 @@
 if (${env:COVERAGE_BUILD} -ne 0) {
   $codecov_root_folder = ${env:APPVEYOR_BUILD_FOLDER} -replace "\\", "/"
-  $codecov_coverage_file = "${codecov_root_folder}/target/site/jacoco/jacoco.xml"
+  $rle_codecov_coverage_file = "${codecov_root_folder}/rle/target/site/jacoco/jacoco.xml"
+  $jewellery_and_stones_codecov_coverage_file = "${codecov_root_folder}/jewelery-and-stones/target/site/jacoco/jacoco.xml"
   Write-Host "Sending coverage data to Codecov"
-  appveyor-retry codecov --required --token "${env:CODECOV_TOKEN}" --file "${codecov_coverage_file}" --root "${codecov_root_folder}" -X gcov;
+  appveyor-retry codecov --required --token "${env:CODECOV_TOKEN}" --file "${rle_codecov_coverage_file}" --file "${jewellery_and_stones_codecov_coverage_file}" --root "${codecov_root_folder}" -X gcov;
   if (${LastExitCode} -ne 0) {
     throw "Failed to send coverage data to Codecov"
   }
--- scripts/travis/codecov.sh
+++ scripts/travis/codecov.sh
@@ -6,11 +6,13 @@ set -e
 source "${TRAVIS_BUILD_DIR}/scripts/travis/travis_retry.sh"
 
 if [[ "${COVERAGE_BUILD}" -ne 0 ]]; then
-  codecov_coverage_file="${TRAVIS_BUILD_DIR}/target/site/jacoco/jacoco.xml"
+  rle_codecov_coverage_file="${TRAVIS_BUILD_DIR}/rle/target/site/jacoco/jacoco.xml"
+  jewellery_and_stones_codecov_coverage_file="${TRAVIS_BUILD_DIR}/jewelery-and-stones/target/site/jacoco/jacoco.xml"
   echo "Sending coverage data to Codecov"
   travis_retry codecov \
     --required \
     --token "${CODECOV_TOKEN}" \
-    --file "${codecov_coverage_file}" \
+    --file "${rle_codecov_coverage_file}" \
+    --file "${jewellery_and_stones_codecov_coverage_file}" \
     --root "${TRAVIS_BUILD_DIR}" -X gcov
 fi
