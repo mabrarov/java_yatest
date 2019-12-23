b4c2f3a04431a463df8d574879ad02833dd1a5d5
Marat Abrarov <abrarov@gmail.com>
2019-12-23 12:13:19 +0300

    Initial version of Java Enum emulator

--- deduplicate/src/main/java/org/mabrarov/yatest/deduplicate/Main.java
+++ deduplicate/src/main/java/org/mabrarov/yatest/deduplicate/Main.java
@@ -16,93 +16,92 @@
 package org.mabrarov.yatest.deduplicate;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
-  private static final int MAX_ITEMS = 1000000;
-
   public static void main(String[] args) throws IOException {
-    boolean first = true;
-    int lastValue = 0;
-    int itemCount = readInt(System.in);
-    if (itemCount < 0 || itemCount > MAX_ITEMS) {
-      throw new IllegalArgumentException("number of items should be [0, " + MAX_ITEMS + "]");
+    System.out.println("Iterating over all currency");
+    for (final Currency currency : Currency.all()) {
+      System.out.println("Ordinal of " + currency + " is " + currency.ordinal());
+      System.out.println("Name of " + currency + " is " + currency.name());
     }
-    int item;
-    while (itemCount > 0) {
-      item = readInt(System.in);
-      if (!first && lastValue > item) {
-        throw new IllegalArgumentException(
-            "items should be non-decreasing, previous item " + lastValue + " current item " + item);
-      }
-      if (first || lastValue != item) {
-        first = false;
-        printInt(item, System.out);
-      }
-      lastValue = item;
-      --itemCount;
-    }
+    System.out.println(Currency.get(Currency.CAD.ordinal()));
+    System.out.println(Currency.get(Currency.CAD.name()));
   }
 
-  private static int readInt(InputStream stream) throws IOException {
-    boolean first = true;
-    int sign = 1;
-    long abs = 0;
-    long value = 0;
-    for (; ; ) {
-      final int c = stream.read();
-      if (c == '\n' || c == -1) {
-        break;
-      }
-      if (c == '-' || c == '+') {
-        if (!first) {
-          throw new IllegalArgumentException("sign is allowed before 1st digit only");
+}
+
+final class Currency {
+
+  public static final Currency USD = new Currency();
+  public static final Currency CAD = new Currency();
+  public static final Currency RUB = new Currency();
+
+  private static final List<Currency> allCurrency;
+  private static final Map<String, Currency> nameToCurrency;
+
+  static {
+    final List<Currency> allCurrencyTemp = new ArrayList<>();
+    final Map<String, Currency> nameToCurrencyTemp = new HashMap<>();
+    int ordinal = 0;
+    for (final Field f : Currency.class.getDeclaredFields()) {
+      if (Modifier.isStatic(f.getModifiers()) && f.getType().equals(Currency.class)) {
+        Currency currency;
+        try {
+          currency = (Currency) f.get(null);
+        } catch (IllegalAccessException e) {
+          throw new AssertionError(e);
         }
-        sign = c == '-' ? -1 : 1;
-        continue;
-      }
-      if (c == ' ' || c == '\t') {
-        continue;
-      }
-      if (c < '0' || c > '9') {
-        throw new IllegalArgumentException("only decimal digits are allowed");
-      }
-      first = false;
-      abs = abs * 10 + (c - '0');
-      value = abs * sign;
-      if (value > Integer.MAX_VALUE) {
-        throw new IllegalArgumentException("32 int overflow");
-      }
-      if (value < Integer.MIN_VALUE) {
-        throw new IllegalArgumentException("32 int underflow");
+        if (currency == null) {
+          throw new AssertionError("Static fields of Currency type should be not null");
+        }
+        String name = f.getName();
+        currency.ordinal = ordinal++;
+        currency.name = name;
+        allCurrencyTemp.add(currency);
+        nameToCurrencyTemp.put(name, currency);
       }
     }
-    return (int) value;
+    allCurrency = Collections.unmodifiableList(allCurrencyTemp);
+    nameToCurrency = Collections.unmodifiableMap(nameToCurrencyTemp);
   }
 
-  private static void printInt(int value, OutputStream stream) throws IOException {
-    if (value >= 0) {
-      printNonNegativeInt(value, stream);
-    } else {
-      stream.write('-');
-      printNonNegativeInt(-((long) value), stream);
-    }
-    stream.write('\n');
-    stream.flush();
+  private int ordinal;
+  private String name;
+
+  private Currency() {
   }
 
-  private static void printNonNegativeInt(long value, OutputStream stream) throws IOException {
-    if (value == 0) {
-      stream.write('0');
-      return;
-    }
-    long prev = value / 10;
-    if (prev != 0) {
-      printNonNegativeInt(prev, stream);
-    }
-    stream.write((char) ('0' + value % 10));
+  public int ordinal() {
+    return ordinal;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public static List<Currency> all() {
+    return allCurrency;
+  }
+
+  public static Currency get(int ordinal) {
+    return allCurrency.get(ordinal);
+  }
+
+  public static Currency get(String name) {
+    return nameToCurrency.get(name);
+  }
+
+  @Override
+  public String toString() {
+    return name;
   }
 
 }
